c2b8d1050502748bb614f4b634de9f209091e3fe
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-07-25 11:01:45 -0300

    Data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Hello</h1>
+  <h1>Hello {{ person }}</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
-export default defineComponent({});
+export default defineComponent({
+  data() {
+    return {
+      person: "s",
+    };
+  },
+});
 </script>
