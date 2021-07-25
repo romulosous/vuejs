cf055e5d97afe8479acb9f255002c27336bce20d
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-07-25 11:17:15 -0300

    methods

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <h1>{{ name }} {{ lastname }}</h1>
-  <h1>{{ fullName }}</h1>
+  <h2>{{ name }} {{ lastname }}</h2>
+  <h2>{{ fullName }}</h2>
+
+  <h2>{{ count }}</h2>
+  <button @click="increment">Incrementar</button>
+  <button @click="decrease">Diminuir</button>
+  <button @click="reset">Resetar</button>
 </template>
 
 <script lang="ts">
@@ -11,11 +16,23 @@ export default defineComponent({
     return {
       name: "Romim",
       lastname: "Carvalho",
+      count: 0,
     };
   },
   computed: {
     fullName(): string {
       return `${this.name} ${this.lastname}`;
+    },
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrease() {
+      this.count--;
+    },
+    reset() {
+      this.count = 0;
     },
   },
 });
